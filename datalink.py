80cdcaec596074a2e5860175e5d5c945e4bc78e6
Your Name <you@example.com>
2025-09-02 17:42:42 +0900

    testing

--- datalink.py
+++ datalink.py
@@ -4,14 +4,16 @@ import json
 from typing import Dict
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import pandas as pd
+# import pandas as pd
 
 stations: Dict = {
     'henderson inlet': '9446752',
     'tacoma': '9446484',
-    'tacoma narrows': '9446486'
+    'tacoma narrows': '9446486',
+    'gig harbor': '9446369',
+    'yoman point': '9446705'
 }
-station = 'tacoma'
+station = 'yoman point'
 
 product = 'predictions'
 time_type = 'lst_ldt'
@@ -26,14 +28,14 @@ datum = 'MLLW'
 api_url = f'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date={begin_date}&end_date={end_date}&station={stations[station]}&product={product}&datum={datum}&time_zone={time_type}&units={data_units}&format={data_format}'
 
 
-def get_data(api_url: str=api_url) -> dict:
+def get_data(api_url: str=api_url) -> Dict:
     try:
         with urllib.request.urlopen(api_url) as url:
             data = json.load(url)
             return data
     except Exception as e:
         print(e)
-
+    return {}
 
 predictions = get_data(api_url)['predictions']
 
@@ -46,8 +48,6 @@ for dictionary in predictions:
             times.append(datetime.strptime(value, '%Y-%m-%d %H:%M'))
         else:
             heights.append(float(value))
-
-
 
 plt.plot(times, heights)
 ax = plt.gca()
